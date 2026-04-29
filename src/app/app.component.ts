58c418720e3b5b25238bae354f63443545caea6b
saizakapps <saibalaji315@gmail.com>
2026-04-29 20:24:50 +0530

    PWA fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UpdateNotificationComponent } from './shared/components/update-notification/update-notification.component';
+import { InstallPromptComponent } from './shared/components/install-prompt/install-prompt.component';
+import { IosInstallComponent } from './shared/components/ios-install/ios-install.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<router-outlet />',
+  imports: [RouterOutlet, UpdateNotificationComponent, InstallPromptComponent, IosInstallComponent],
+  template: `
+    <router-outlet />
+    <app-update-notification />
+    <app-install-prompt />
+    <app-ios-install />
+  `,
 })
 export class AppComponent {}
