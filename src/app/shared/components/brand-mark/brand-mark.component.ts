539a1fd10ecb80932d6ce33d0d92b1323e31412c
saizakapps <saibalaji315@gmail.com>
2026-04-29 18:13:53 +0530

    Logo added and UI Bug fixes

--- src/app/shared/components/brand-mark/brand-mark.component.ts
+++ src/app/shared/components/brand-mark/brand-mark.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-brand-mark',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './brand-mark.component.html',
   styleUrls: ['./brand-mark.component.scss'],
 })
 export class BrandMarkComponent {
   @Input() size: number = 36;
+  @Input() link: string = '/';
 
   get iconSize(): number {
     return this.size * 0.5;
